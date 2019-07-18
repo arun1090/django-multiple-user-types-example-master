10fd5bfe4bc4f10e1cb4a6437a35bf8d496f9605
= <12bee1090@gmail.com>
2019-07-18 14:58:45 +0530

    commit for django school

---
+++ django-multiple-user-types-example-master/django-multi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\arun\Desktop\django_practice\django-multiple-user-types-example-master\django-multiple-user-types-example-master\django-multi\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
